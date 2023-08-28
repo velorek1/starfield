109b16e24b8cb33325b74894ba7e9888487c2b0d
Raúl Sanguiao <velorek1@gmail.com>
2023-08-28 14:59:53 +0200

    code simplified

--- nstar.c
+++ nstar.c
@@ -12,30 +12,20 @@
 #include <time.h>
 #include <SDL2/SDL.h>
 
-/*
-#define res		2			//0=160x120 1=360x240 2=640x480
-#define pixelScale 	4/res
-#define SW		160*res			//screen width
-#define SH		120*res			//screen height
-*/
-//#define pixelScale    4
-#define SW		640	//half of screen width
-#define SH		480	//half of screen height
+#define SW		640	//screen width
+#define SH		480	//screen height
 #define SW2		(SW/2)	//half of screen width
 #define SH2		(SH/2)	//half of screen height
 #define TRUE		1
 #define FALSE		0
 #define MAX_KEY 	1000
-//#define SDLW          (SW*pixelScale)         //SDL window width
-//#define SDLH          (SH*pixelScale)         //SDl window height
 #define MAX_STARS	63
 //--------------------------------------------------------------------------------
 typedef short BOOL;
 
 typedef struct {
-	int w, s, a, d;		//move up, down, left, right
-	int sl, sr;		//strafe left, right 
-	int m;			//move up, down, look up, down
+	int w, s, a, d;		// increase spead, decrease, activate streak
+	int m;			// fullscreen
 } keys;
 keys K;
 
@@ -79,7 +69,7 @@ void Main_Loop();
 void UpdateGame();
 void message();
 
-//-> Entry Point
+//Functions
 
 int randm(unsigned x)
 {
@@ -110,9 +100,6 @@ BOOL InitVideo()
 	    SDL_CreateWindow("STARS Z-Demo v0.1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SW, SH,
 			     SDL_WINDOW_SHOWN);
 	ren1 = SDL_CreateRenderer(win1, -1, 0);
-	//SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, 0);
-	//SDL_RenderSetLogicalSize(ren1, 2048, 1024); 
-	//SDL_RenderSetScale(ren1,pixelScale-2,pixelScale-2);
 	return (ren1 != NULL) && (win1 != NULL);
 }
 
@@ -156,12 +143,6 @@ void KeysUp()
 	if (Key(SDLK_m)) {
 		K.m = 1;
 	}
-	if (Key(SDLK_COMMA)) {
-		K.sr = 1;
-	}
-	if (Key(SDLK_PERIOD)) {
-		K.sl = 1;
-	}
 }
 
 void KeysDown()
@@ -180,24 +161,11 @@ void KeysDown()
 	}
 	if (Key(SDLK_m) == FALSE) {
 		K.m = 0;
-	}
-	
-	if (Key(SDLK_COMMA) == FALSE) {
-		K.sr = 0;
-	}
-	if (Key(SDLK_PERIOD) == FALSE) {
-		K.sl = 0;
-	}
-	
+	}	
 }
 
 void HandleKey(long Sym, BOOL Down)
 {
-	//if (Sym == SDLK_UP) Sym = SDL_SCANCODE_UP;
-	//if (Sym == SDLK_DOWN) Sym = SDL_SCANCODE_DOWN;
-	// if (Sym == SDLK_LEFT) Sym = SDL_SCANCODE_LEFT;
-	// if (Sym == SDLK_RIGHT) Sym = SDL_SCANCODE_RIGHT;
-	// if (Sym == SDLK_SPACE) Sym = SDL_SCANCODE_SPACE;
 	if ((Sym >= 0) && (Sym <= MAX_KEY)) {
 		KeyState[Sym] = Down;
 		if (Sym == SDLK_ESCAPE)
@@ -250,7 +218,7 @@ void pixel(int x, int y, int c, int alpha)	//draw a pixel at x/y with rgb
 	}			//White       
 	newpoint = (SDL_Point *) malloc(sizeof (SDL_Point));	//new pointer
 	newpoint->x = x;
-	newpoint->y = y;	// convertPointY(y);i
+	newpoint->y = y;
 	SDL_SetRenderDrawBlendMode(ren1, SDL_BLENDMODE_BLEND);
 	SDL_SetRenderDrawColor(ren1, rgb[0], rgb[1], rgb[2], alpha);
 	SDL_RenderDrawPoints(ren1, newpoint, 1);
@@ -272,7 +240,6 @@ void clearBackground()
 			pixel(x, y, 0, 255);
 		}		//clear background color
 	}
-	//SDL_RenderPresent(ren1);
 }
 
 void initStars()
@@ -285,7 +252,6 @@ void initStars()
 		px[i] = 0;
 		py[i] = 0;
 		szv[i] = rand_float(0.5, 5);
-		//printf("X:%f /t Y:%f /t Z:%f /t px: %d /t py: %d\n", sx[i], sy[i],sz[i],px[i],py[i]);
 	}
 }
 
@@ -296,19 +262,18 @@ void newGame()
 }
 
 void message(){
-   // Blue message
     
     // Reset color to default
     printf("\033[0m");
     
-    // Red message
     printf("\033[1;31m[nstar]\033[0m coded by\033[90m velorek\n");
     printf("\033[1;97mKeys:\n");
     printf("\033[0m");
     printf("\033[1;94mw \033[0m increase speed\n");
-    printf("\033[1;94md \033[0m decrease speed\n");
+    printf("\033[1;94ms \033[0m decrease speed\n");
     printf("\033[1;94ma \033[0m toggle streak\n");
     printf("\033[1;94mm \033[0m toggle fullscreen\n");
+    printf("\033[1;94md \033[0m show SPEED\n");
     printf("\033[1;94mESC \033[0m quit\n");
     
     // Reset color to default
@@ -320,18 +285,22 @@ void UpdateGame()
 {
 	if (K.w == 1) {
 		SPEED++;
-		//printf("%f\n", SPEED);
 		K.w == 0;
 	}
 	if (K.s == 1) {
 		SPEED--;
-		//printf("%f\n", SPEED);
 		K.s == 0;
 	}
 	if (K.m == 1) {
 		ToggleFullscreen(win1);
 		K.m == 0;
 	}
+	if (K.d == 1) {
+		//Show message
+		K.d == 0;
+		printf("Current SPEED : %f\n",SPEED);
+	}
+
 	if (K.a == 1) {
 		if (streak)
 			streak = FALSE;
@@ -349,32 +318,20 @@ void draw3D()
 	float length = 0;
 	clearBackground();
 	for (int i = 0; i < MAX_STARS; i++) {
-		// pixel(SW2,SH2,0,255);
-		//if (i>0) pixel(px[i-1]-1,py[i-1]-1,1);
 		sz[i] = (sz[i] - szv[i]) - SPEED;
-		//if (sz[i]!=0){
-		//px[i] = (sx[i]/(sz[i]))*100 + (SW2);
-		//py[i] = (sy[i]/(sz[i]))*100 + (SH2);
 		x = (sx[i] / (sz[i])) * 100 + (SW2);
 		y = (sy[i] / (sz[i])) * 100 + (SH2);
-		//b = ( (255/5) * szv[i]) * (1000 / sz[i]);
 		b = 255 - (sz[i] * (255. / 1000.));
-		//b = 255-(sz[i]);
 		pixel(px[i], py[i], 1, b);
 
-		//}
 		if ((sz[i] < 1) || (px[i] < 0 && px[i] > SW)
 		    && (py[i] < 0 && py[i] > SH)) {
 			sx[i] = rand_num(-500, 500);
 			sy[i] = rand_num(-500, 500);
 			sz[i] = rand_num(100, 1000);
-			//printf("%d:%d\n",sran,signus);
 			szv[i] = rand_float(0.5, 5);
-			//printf("X:%f /t Y:%f /t Z:%f /t px: %d /t py: %d  ; szv:%f\n", sx[i], sy[i],sz[i],px[i],py[i],szv[i]);
 		}
 
-		// b = 255-(sz[i]);
-		//b = 255-(sz[i]*(255./1000.));
 		//calculate the length of the streak
 		xd = x - px[i];
 		yd = y - py[i];
@@ -386,18 +343,13 @@ void draw3D()
 		}
 		pixel(px[i], py[i] + 1, 1, b);
 		//draw streak
-		//draw_line(ren1,x,y,px[i],py[i],255,255,255,255);
 		if (sz[i] > 1 && (px[i] > 1 && px[i] < SW)
 		    && (py[i] > 1 && py[i] < SH)
 		    && (x > 1 && x < SW) && (y > 1 && y < SH) && length < 50
 		    && streak) {
 			b = (5000 * szv[i]) / sz[i];
-			//if (length > 1) b = (b/length);
 			draw_line(ren1, x, y, px[i], py[i], 255, 255, 255, b);
-			//draw_line(ren1,px[i],py[i],x,y,255,255,255,100);
 
-			//printf("%d:%d:%d:%d:%f:%d\n", x, y, px[i], py[i],
-			   //    length, b);
 		}
 		px_old[i] = px[i];
 		py_old[i] = py[i];
